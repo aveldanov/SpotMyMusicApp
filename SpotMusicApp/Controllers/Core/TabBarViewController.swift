ff5d7f94e5da97162256e9c03a1069ace08efd23
Anton Veldanov <anton.veldanov@gmail.com>
2021-06-18 18:51:09 -0700

    iOS

--- SpotMusicApp/Controllers/Core/TabBarViewController.swift
+++ SpotMusicApp/Controllers/Core/TabBarViewController.swift
@@ -12,6 +12,27 @@ class TabBarViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let vc1 = HomeViewController()
+        let vc2 = SearchViewController()
+        let vc3 = LibraryViewController()
+        
+        vc1.navigationItem.largeTitleDisplayMode = .always
+        vc2.navigationItem.largeTitleDisplayMode = .always
+        vc3.navigationItem.largeTitleDisplayMode = .always
+        
+        let nav1 = UINavigationController(rootViewController: vc1)
+        let nav2 = UINavigationController(rootViewController: vc2)
+        let nav3 = UINavigationController(rootViewController: vc3)
+        
+        nav1.navigationBar.prefersLargeTitles = true
+        nav2.navigationBar.prefersLargeTitles = true
+        nav3.navigationBar.prefersLargeTitles = true
+        
+        setViewControllers([nav1, nav2, nav3], animated: false)
+        
+        vc1.title = "Browse"
+        vc2.title = "Search"
+        vc3.title = "Library"
     }
     
 
