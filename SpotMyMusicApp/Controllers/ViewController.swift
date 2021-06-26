b33458d30a8b9f021ca096f77d424434b36ad825
Anton Veldanov <anton.veldanov@gmail.com>
2021-06-26 13:30:33 -0700

    iOS

--- SpotMyMusicApp/Controllers/ViewController.swift
+++ SpotMyMusicApp/Controllers/ViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Home"
+        view.backgroundColor = .red
+        
     }
-
-
+    
+    
 }
 
