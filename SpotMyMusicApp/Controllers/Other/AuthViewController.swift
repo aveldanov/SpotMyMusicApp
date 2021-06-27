7eb4264c29a1e08bac2f3c8d4a3df005fbc9988d
Anton Veldanov <anton.veldanov@gmail.com>
2021-06-26 17:31:16 -0700

    iOS

--- SpotMyMusicApp/Controllers/Other/AuthViewController.swift
+++ SpotMyMusicApp/Controllers/Other/AuthViewController.swift
@@ -55,8 +55,17 @@ class AuthViewController: UIViewController, WKNavigationDelegate {
         })?.value else{
             return
         }
-        
+        webView.isHidden = true
         print("Code\(code)")
+        
+        AuthManager.shared.exchangeCodeForToken(code: code) { success in
+            
+            DispatchQueue.main.async {
+                self.navigationController?.popToRootViewController(animated: true)
+                self.completion?(success)
+            }
+
+        }
     }
 
 
--- SpotMyMusicApp/Helpers/AuthManager.swift
+++ SpotMyMusicApp/Helpers/AuthManager.swift
@@ -15,6 +15,7 @@ final class AuthManager{
     struct Constants {
         static let clientID = "cab2989e12fa45e69f28440e52f53172"
         static let clientSecret = "67a4973e4394471a86ca3da29a4a4734"
+        static let tokenAPIURL = "https://accounts.spotify.com/api/token"
     }
     
     
@@ -50,6 +51,49 @@ final class AuthManager{
     
     public func exchangeCodeForToken(code: String, completion: @escaping ((Bool)->(Void))){
         //Get token
+        guard let url = URL(string: Constants.tokenAPIURL) else {
+            return
+        }
+        // POST components
+        var components = URLComponents()
+        components.queryItems = [
+            URLQueryItem(name: "grant_type", value: "authorization_code"),
+            URLQueryItem(name: "code", value: "code"),
+            URLQueryItem(name: "redirect_uri", value: "https://www.intuit.com/")
+        ]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpBody = components.query?.data(using: .utf8)
+        let basicToken = Constants.clientID+":"+Constants.clientSecret
+        let data = basicToken.data(using: .utf8)
+        guard let base64String = data?.base64EncodedString() else{
+            completion(false)
+            print("Base64 problems")
+            return
+        }
+        
+        
+        request.setValue("Authorization", forHTTPHeaderField: "Authorization")
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                completion(false)
+                return
+            }
+            
+            do{
+                let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                print("SUCCESS",json)
+            }catch{
+                print(error.localizedDescription)
+                completion(false)
+            }
+            
+            
+        }.resume()
+        
     }
     
     public func refreshAccessToken(){
