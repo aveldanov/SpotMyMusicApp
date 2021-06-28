f5b887431b0177f701b2ba3b17dd86deec131c1c
Anton Veldanov <anton.veldanov@gmail.com>
2021-06-27 18:49:05 -0700

    iOS

--- SpotMyMusicApp/Helpers/APICaller.swift
+++ SpotMyMusicApp/Helpers/APICaller.swift
@@ -12,9 +12,38 @@ final class APICaller {
     static let shared = APICaller()
     private init(){}
     
+    struct Constants {
+        static let baseAPIURL = "https://api.spotify.com/v1/me" // from API page
+    }
+    
+    enum APIError: Error{
+        case failedToGetData
+        
+    }
+    
+    
     public func getCurrentUserProfile(completion: @escaping (Result<UserProfile,Error>)->(Void)){
         
-        
+        createRequest(with: URL(string: Constants.baseAPIURL+"/me"), type: .GET) { baseRequest in
+            URLSession.shared.dataTask(with: baseRequest) { data, response, error in
+                guard let data = data, error == nil else{
+                    completion(.failure(APIError.failedToGetData))
+                    return
+                }
+                
+                do{
+                    
+                    let result = JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                    print(result)
+                }catch{
+                    
+                    completion(.failure(error))
+                    
+                }
+                
+                
+            }
+        }
     }
     
     enum HttpMethod: String{
