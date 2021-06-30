63f9f2b32500905a8831998e36da884d16bf06fc
Anton Veldanov <anton.veldanov@gmail.com>
2021-06-29 18:14:29 -0700

    iOS

--- SpotMyMusicApp/Controllers/Other/ProfileViewController.swift
+++ SpotMyMusicApp/Controllers/Other/ProfileViewController.swift
@@ -7,14 +7,37 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let tableView: UITableView = {
+       let tableView = UITableView()
+        tableView.isHidden = true //in case there is no data
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return tableView
+    }()
+    
+    private var models = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Profile"
+        view.addSubview(tableView)
         fetchProfile()
         view.backgroundColor = .systemBackground
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+    
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    
+    
     
     private func fetchProfile(){
         APICaller.shared.getCurrentUserProfile { result in
@@ -23,7 +46,7 @@ class ProfileViewController: UIViewController {
                 case .success(let model):
                     self.updateUI(with: model)
                 case .failure(let error):
-                    print(error.localizedDescription)
+                    print("[ProfileVC] Profile Error:",error.localizedDescription)
                     self.failedToGetProfile()
                 }
             }
@@ -31,9 +54,27 @@ class ProfileViewController: UIViewController {
     }
     
     private func updateUI(with model: UserProfile){
+        tableView.isHidden = false
+        //configure table models
+        models.append("Full name: \(model.display_name)")
+        models.append("Email: \(model.email)")
+        models.append("User id: \(model.id)")
+        models.append("Plan: \(model.product)")
+        createTableHeader(with: model.images.first?.url)
+        
+        tableView.reloadData()
+        
+    }
+    
+    private func createTableHeader(with urlString: String?){
+        guard let urlString = urlString, let url = URL(string: urlString) else {
+            return
+        }
         
         
     }
+    
+    
     
     private func failedToGetProfile(){
         let label = UILabel(frame: .zero)
@@ -42,6 +83,23 @@ class ProfileViewController: UIViewController {
         label.textColor = .secondaryLabel
         view.addSubview(label)
         label.center = view.center
+    }
+    
+     //MARK: TableView
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = models[indexPath.row]
+        cell.selectionStyle = .none
+        
+        
+        return cell
     }
     
 }
--- SpotMyMusicApp/Helpers/APICaller.swift
+++ SpotMyMusicApp/Helpers/APICaller.swift
@@ -38,6 +38,7 @@ final class APICaller {
                     
                     let result = try JSONDecoder().decode(UserProfile.self, from: data)
                     print("[APICaller] result",result)
+                    completion(.success(result))
                 }catch{
                     print(error.localizedDescription)
                     completion(.failure(error))
