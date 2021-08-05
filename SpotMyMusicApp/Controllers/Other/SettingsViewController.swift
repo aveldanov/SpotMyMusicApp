b7a9ed1c548b07d260ea59000306efb678b3ee52
Anton Veldanov <anton.veldanov@gmail.com>
2021-08-05 15:55:43 -0700

    iOS

--- SpotMyMusicApp/Controllers/Other/SettingsViewController.swift
+++ SpotMyMusicApp/Controllers/Other/SettingsViewController.swift
@@ -34,4 +34,30 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
         
     }
     
+    
+     //MARK: TableView
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+            fatalError("No Cell")
+        }
+        cell.textLabel?.text = "foo"
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // call handler for the cell
+    }
+    
 }
