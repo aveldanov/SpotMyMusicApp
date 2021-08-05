af7f2d088c740e3a70665488be11d76fe2bc623e
Anton Veldanov <anton.veldanov@gmail.com>
2021-08-05 15:06:59 -0700

    iOS

--- SpotMyMusicApp/Helpers/APICaller.swift
+++ SpotMyMusicApp/Helpers/APICaller.swift
@@ -12,18 +12,62 @@ final class APICaller{
     static let shared = APICaller()
     private init(){} //This prevents others from using the default '()' initializer for this class. no one can create: let foo = APICaller()
     
+    struct Constants{
+       static let baseAPIURL = "https://api.spotify.com/v1"
+        
+    }
+    
+    enum APIError: Error{
+        case failedToGetData
+    }
+    
     public func getCurrentUserProfile(completion: @escaping (Result<UserProfile, Error>) -> Void){
         
-        AuthManager.shared.withValidToken { token in
-            <#code#>
+        createRequest(
+            with: URL(string: Constants.baseAPIURL + "/me"),
+            type: .GET) { baseRequest in
+            
+            URLSession.shared.dataTask(with: baseRequest) { data, response, error in
+                guard let data = data, error == nil else{
+                    completion(.failure(APIError.failedToGetData))
+                    return
+                }
+                
+                do{
+                    let result = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                    print(result)
+                    
+                }catch{
+                    
+                    completion(.failure(error))
+                }
+                
+                
+                
+            }.resume()
         }
+
     }
     
      //MARK: Generic request for regular API Call
+    enum HTTPMethod: String{
+        case GET
+        case POST
+    }
     
-    
-    private createRequest()-> URLRequest{
-        
+    private func createRequest(with url: URL?,type: HTTPMethod, completion: @escaping (URLRequest) -> Void){
+        // URLRequest is URL + HTTPS Params in the call, like header, etc!
+        AuthManager.shared.withValidToken { token in
+            guard let apiURL = url else {
+                return
+            }
+            print(token)
+            var request = URLRequest(url: apiURL)
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+            request.httpMethod = type.rawValue
+            request.timeoutInterval = 30
+            completion(request)
+        }
     }
     
     
