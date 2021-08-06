505e01c69a4c34f1c255cb7a7f5cb454c1df68eb
Anton Veldanov <anton.veldanov@gmail.com>
2021-08-05 17:58:44 -0700

    iOS

--- SpotMyMusicApp/Controllers/Other/ProfileViewController.swift
+++ SpotMyMusicApp/Controllers/Other/ProfileViewController.swift
@@ -7,39 +7,68 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.isHidden = true
+        return tableView
+    }()
+    
+    private var models = [String]()
+    
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Profile"
+        view.addSubview(tableView)
         view.backgroundColor = .systemBackground
         fetchProfile()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+        
+    }
+    
     
     private func fetchProfile(){
         APICaller.shared.getCurrentUserProfile { result in
-            print("result",result)
             DispatchQueue.main.async {
-                self.failedToGetProfile()
-//                switch result{
-//                case .success(let model):
-//                    break
-//                case .failure(let error):
-//                    print(error.localizedDescription)
-//                    self.failedToGetProfile()
-//                }
+                switch result{
+                case .success(let model):
+                    break
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    self.failedToGetProfile()
+                }
             }
         }
     }
     
     private func updateUI(with model: UserProfile){
+        tableView.isHidden = false
+        // configure table models
+        models.append("Full name:\(model.display_name)")
+        models.append("Email:\(model.email)")
+        models.append("UserID:\(model.id)")
+        models.append("Plan:\(model.product)")
         
-        
+        tableView.reloadData()
     }
     
     
     private func failedToGetProfile(){
-        print("YO")
         let label = UILabel(frame: .zero)
         label.text = "Failed to load profile"
         label.sizeToFit()
@@ -47,4 +76,25 @@ class ProfileViewController: UIViewController {
         view.addSubview(label)
         label.center = view.center
     }
+    
+    
+    
+    
+     //MARK: tableView
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) else {
+            fatalError("No Cell")
+        }
+        cell.selectionStyle = .none
+        cell.textLabel?.text = "FOO"
+        
+        return cell
+    }
+    
+    
 }
