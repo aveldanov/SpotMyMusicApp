5dc7d1f47353ab0ba32bdd258173e71e3caab425
Anton Veldanov <anton.veldanov@gmail.com>
2021-08-06 06:54:32 -0700

    iOS

--- SpotMyMusicApp/Controllers/Other/ProfileViewController.swift
+++ SpotMyMusicApp/Controllers/Other/ProfileViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
@@ -60,8 +61,32 @@ class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewD
         models.append("UserID: \(model.id)")
         models.append("Plan: \(model.product)")
         
+        
+        createTableHeader(with: model.images.first?.url)
+        
         tableView.reloadData()
     }
+    
+    private func createTableHeader(with string: String?){
+        guard let urlString = string, let url = URL(string: urlString) else {
+            return
+        }
+        
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: view.width, height: view.width/1.5))
+        
+        let imageSize: CGFloat = headerView.height/2
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: imageSize, height: imageSize))
+        headerView.addSubview(imageView)
+        imageView.center = headerView.center
+        imageView.sd_setImage(with: url, completed: nil)
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = imageSize/2
+        
+        tableView.tableHeaderView = headerView
+        
+    }
+    
     
     
     private func failedToGetProfile(){
